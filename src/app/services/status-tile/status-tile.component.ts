4804ab7007b8c3e65266278ec1c025800a2b65d7
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-07-03 18:06:25 +0200

    Some cleanup and icons

--- src/app/services/status-tile/status-tile.component.ts
+++ src/app/services/status-tile/status-tile.component.ts
@@ -5,15 +5,15 @@ import { Component, Input, OnInit } from '@angular/core';
   template: `
     <mat-card class="Card">
       <mat-card-header>
-        <div mat-card-avatar><app-blinker></app-blinker></div>
-        <mat-card-title>{{title}}</mat-card-title>
-        <mat-card-subtitle>
+        <div mat-card-avatar class="Avatar"><img alt="icon" class="Icon" src="{{ icon }}"></div>
+        <mat-card-title>{{title}} <app-blinker></app-blinker></mat-card-title>
+        <mat-card-subtitle class="Subtitle">
           Service address:
           <a mat-button href="{{ href }}">{{ href }}</a>
         </mat-card-subtitle>
       </mat-card-header>
       <mat-card-content>
-        <p>Serves configuration and applies actions</p>
+        <ng-content></ng-content>
       </mat-card-content>
       <mat-card-actions class="button-row" *ngIf="showActions">
         <button mat-flat-button color="accent">Start</button>
@@ -29,6 +29,8 @@ export class StatusTileComponent implements OnInit {
   @Input() showActions: boolean;
   @Input() title: string;
   @Input() href: string;
+  @Input() description: string;
+  @Input() icon: string;
 
   constructor() {
   }
