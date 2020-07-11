97356fa11cbb75faa786759350b00077a9f36dcf
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-07-11 19:42:14 +0200

    Validator

--- src/app/torrent-client/components/add-item/add-torrent-dialog.component.ts
+++ src/app/torrent-client/components/add-item/add-torrent-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { magnetValidator } from "../../../shared/validators/magnet.validator";
 
 export interface IAddTorrentDialogData {
   categories: string[];
@@ -34,16 +35,28 @@ export class AddTorrentDialogComponent implements OnInit {
     const {categories, directories, magnet, size, title} = this.data;
     this.directories = directories;
     this.categories = categories;
-    this.addTorrentForm = new FormGroup(
-      {
-        category: new FormControl('', Validators.required),
-        directory: new FormControl('', Validators.required),
-        magnet: new FormControl(magnet, Validators.required),
-        size: new FormControl(size, Validators.required),
-        title: new FormControl(title, Validators.required),
-        start: new FormControl(true, Validators.required),
-      }
-    )
+    if (magnet || size || title) {
+      this.addTorrentForm = new FormGroup(
+        {
+          category: new FormControl('', Validators.required),
+          directory: new FormControl('', Validators.required),
+          magnet: new FormControl({value: magnet, disabled: !!magnet}, Validators.required),
+          size: new FormControl({value: size, disabled: !!size}, Validators.required),
+          title: new FormControl({value: title, disabled: !!title}, Validators.required),
+          start: new FormControl(true, Validators.required),
+        }
+      )
+    }
+    else {
+      this.addTorrentForm = new FormGroup(
+        {
+          category: new FormControl('', Validators.required),
+          directory: new FormControl('', Validators.required),
+          magnet: new FormControl('', [Validators.required, magnetValidator]),
+          start: new FormControl(true, Validators.required),
+        }
+      )
+    }
   }
 
 }
