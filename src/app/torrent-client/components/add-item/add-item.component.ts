09900ceea1b3c00b4ec7b12ce3fe29d87473c16d
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-07-12 21:42:41 +0200

    Added elevation and scaling to FAB

--- src/app/torrent-client/components/add-item/add-item.component.ts
+++ src/app/torrent-client/components/add-item/add-item.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Output } from "@angular/core";
 @Component({
   selector: 'app-add-item-component',
   template: `
-    <button class="Fab" mat-fab color="primary" (click)="showModal.emit()"><mat-icon>add</mat-icon></button>
+    <button class="Fab mat-elevation-z12" mat-fab (click)="showModal.emit()"><mat-icon>add</mat-icon></button>
   `,
   styleUrls: ['./add-item.component.scss'],
 })
