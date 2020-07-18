727e721f3410a95a794d3e21ccae8ea4438c60e0
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-07-18 21:50:37 +0200

    Resolved a todo

--- src/app/torrent-search/components/providers/providers.component.ts
+++ src/app/torrent-search/components/providers/providers.component.ts
@@ -1,10 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IOptions, ITorrentProvider } from "../../state";
 
-/*
-* TODO add a functionality that makes search run on provider changed
-* */
-
 @Component({
   selector: 'app-providers-component',
   templateUrl: './providers.component.html',
--- src/app/torrent-search/containers/providers.container.ts
+++ src/app/torrent-search/containers/providers.container.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { select, Store } from "@ngrx/store";
-import * as torrentSearchSelectors from "../state/torrent-search.selectors";
-import { Observable } from "rxjs";
+import * as selectors from "../state/torrent-search.selectors";
+import { Observable, Subscription } from "rxjs";
 import { IOptions, ITorrentProvider, State } from "../state";
-import * as torrentSearchActions from "../state/torrent-search.actions";
+import * as actions from "../state/torrent-search.actions";
 
 @Component({
   template: `
@@ -16,24 +16,44 @@ import * as torrentSearchActions from "../state/torrent-search.actions";
   `,
   selector: 'app-providers-container'
 })
-export class ProvidersContainer implements OnInit {
+export class ProvidersContainer implements OnInit, OnDestroy {
 
   $enabledProviders: Observable<(ITorrentProvider & { enabled: boolean })[]>;
+  enabledProvidersSub: Subscription;
 
   constructor(private store: Store<State>) {
   }
 
   ngOnInit(): void {
-    this.store.dispatch(new torrentSearchActions.LoadProviders);
-    this.store.dispatch(new torrentSearchActions.LoadEnabledProviders);
+    this.store.dispatch(new actions.LoadProviders);
+    this.store.dispatch(new actions.LoadEnabledProviders);
 
     this.$enabledProviders = this.store.pipe(
-      select(torrentSearchSelectors.getEnabledProvidersBindable),
+      select(selectors.getEnabledProvidersBindable),
     );
+
+    this.enabledProvidersSub = this.store
+      .pipe(select(selectors.getEnabledProviders))
+      .subscribe(
+        () => this.store.pipe(select(selectors.getQuery)
+        ).subscribe(query => {
+          if (query) this.store.dispatch(new actions.PerformSearch(query))
+        }));
+  }
+
+  ngOnDestroy(): void {
+    this.enabledProvidersSub.unsubscribe();
   }
 
   updateEnabledProviders(change: IOptions) {
-    this.store.dispatch(new torrentSearchActions.UpdateEnabledProviders(change));
+    this.store.dispatch(new actions.UpdateEnabledProviders(change));
+    this.store.pipe(
+      select(selectors.getQuery),
+    ).subscribe(
+      next => {
+        if (next) this.store.dispatch(new actions.PerformSearch(next))
+      }
+    )
   }
 
 }
