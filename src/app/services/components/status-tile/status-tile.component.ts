c8f17e1b649592d9103b1f184a52b79283107881
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2021-01-13 19:02:18 +0100

    Fixed urls finally :)

--- src/app/services/components/status-tile/status-tile.component.ts
+++ src/app/services/components/status-tile/status-tile.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { IService } from '../../state/services.reducer';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {environment} from '../../../../environments/environment';
 
 @Component({
   selector: 'app-status-tile',
   templateUrl: './status-tile.component.html',
   styleUrls: [ './status-tile.component.scss' ]
 })
-export class StatusTileComponent {
+export class StatusTileComponent implements OnInit{
   @Input() showActions: boolean;
   @Input() service: IService;
   @Input() icon: string;
+  serviceUrl: SafeUrl;
+  serverUrl = environment.serverIp;
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit(): void {
+    this.serviceUrl = this.sanitizer.bypassSecurityTrustUrl(`http://${environment.serverIp}:${this.service.port}`);
+  }
 }
