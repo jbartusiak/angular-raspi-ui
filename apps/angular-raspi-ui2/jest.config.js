ca19f45e604a12e1ddd4530f7e1b0668594c8ee7
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2021-09-03 19:06:40 +0200

    Move to jest

---
+++ apps/angular-raspi-ui2/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'angular-raspi-ui2',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/angular-raspi-ui2',
+};
